ba56e578def8b8e968b8f9357531a53ab5f249a0
Filip Bradaric <filip.bradaric@inetec.com>
2024-02-26 11:51:05 +0100

    feat: changelog plugin

--- release.config.js
+++ release.config.js
@@ -3,6 +3,8 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/git",
     "@semantic-release/github",
   ],
 };
